47679412b9611b2645160af1da9524fb2cd729d8
glebovitz <gregg@lebovitz.net>
2026-01-19 08:23:45 -0500

    put start loop back in begin

--- sx1262_driver/sx1262_common.py
+++ sx1262_driver/sx1262_common.py
@@ -33,7 +33,7 @@ class SX1262Common:
 
         self.set_packet_type(LORA_MODEM)
         self._fix_resistance_antenna()
-        # self._start_recv_loop()
+        self._start_recv_loop()
         return True
 
     def end(self):
@@ -96,7 +96,7 @@ class SX1262Common:
     
     def start(self, rx_timeout, interval=0.01):
         print(f"starting radio receive {hex(rx_timeout)} ms")
-        self._start_recv_loop(interval)
+        # self._start_recv_loop(interval)
         ok = self.request(rx_timeout)
         if not ok:
             raise RuntimeError("Failed to enter RX mode.")
