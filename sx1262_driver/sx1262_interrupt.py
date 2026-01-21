196f71f94825743a1e66687249fb55b4ceedad11
glebovitz <gregg@lebovitz.net>
2026-01-21 14:53:48 -0500

    allow time for irq to settle experiment 14

--- sx1262_driver/sx1262_interrupt.py
+++ sx1262_driver/sx1262_interrupt.py
@@ -98,7 +98,7 @@ class SX1262Interrupt:
                 if irq:
                     self._handle_irq(irq, None)  # handle IRQ STATUS, read FIFO if RX_DONE
                     self.clear_irq_status(IRQ_ALL)
-                    time.sleep(0.3)  # allow time for processing
+                    time.sleep(0.0003)  # allow time for processing
             self._recv_stopped = True
             print("recv loop stopped")
 
